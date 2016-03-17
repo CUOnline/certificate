a6ebb91628c3a919f25b0746ca3e90c665ae520a
Connor Ford <connor.ford@ucdenver.edu>
2016-03-17 15:04:43 -0600

    Add routes and response headers

--- certificate_app.rb
+++ certificate_app.rb
@@ -3,4 +3,13 @@ require 'wolf'
 class CertificateApp < Wolf::Base
   set :root, File.dirname(__FILE__)
   self.setup
+
+  post '/' do
+    headers 'X-Frame-Options' => "ALLOW_FROM #{settings.canvas_url}"
+  end
+
+  get '/' do
+    headers 'Content-Type' => 'text/xml'
+    slim :lti_config
+  end
 end
