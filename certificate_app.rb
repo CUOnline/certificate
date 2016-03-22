165157a57d6065f29d9863d4eb62df5eeb647626
Connor Ford <connor.ford@ucdenver.edu>
2016-03-22 12:10:51 -0600

    Add certificate requirement lookup

--- certificate_app.rb
+++ certificate_app.rb
@@ -1,11 +1,36 @@
 require 'wolf'
 
 class CertificateApp < Wolf::Base
+  set :quiz_id, 1234
   set :root, File.dirname(__FILE__)
   self.setup
 
   post '/' do
     headers 'X-Frame-Options' => "ALLOW_FROM #{settings.canvas_url}"
+
+    query_string = %{
+      SELECT score, user_dim.name
+      FROM quiz_submission_dim
+      JOIN user_dim
+        ON user_dim.id = quiz_submission_dim.user_id
+      WHERE user_dim.canvas_id = ?
+        AND quiz_submission_dim.quiz_id = ? }
+
+    cursor = settings.db.prepare(query_string)
+    cursor.execute(params['custom_canvas_user_id'], settings.quiz_id)
+
+    scores = []
+    while row = cursor.fetch_hash
+      scores << row['score']
+      name ||= row['name']
+    end
+
+    if scores.select{|s| s.to_i > 90}.any?
+      @pass = true
+      Resque.enqueue(CertificateWorker, name)
+    end
+
+    slim :index
   end
 
   get '/' do
