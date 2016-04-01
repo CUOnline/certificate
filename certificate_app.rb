917e60ac2666844a37fc3f41b08407e6f2e040cc
Connor Ford <connor.ford@ucdenver.edu>
2016-04-01 17:49:07 -0600

    Check quizzes from API, not slow-updating redshift

--- certificate_app.rb
+++ certificate_app.rb
@@ -8,31 +8,22 @@ class CertificateApp < Wolf::Base
   self.setup
 
   post '/' do
-    headers 'X-Frame-Options' => "ALLOW_FROM #{settings.canvas_url}"
+    @course_id = params['custom_canvas_course_id']
+    url = "courses/#{@course_id}/quizzes/#{settings.quiz_id}/submissions"
 
-    query_string = %{
-      SELECT score, user_dim.name
-      FROM quiz_submission_fact
-      JOIN user_dim
-        ON user_dim.id = quiz_submission_fact.user_id
-      WHERE user_dim.canvas_id = ?
-        AND quiz_submission_fact.quiz_id = ? }
-
-    cursor = settings.db.prepare(query_string)
-    cursor.execute(params['custom_canvas_user_id'], settings.quiz_id)
-
-    scores = []
-    while row = cursor.fetch_hash
-      scores << row['score']
-      @name ||= row['name']
+    passed_quizzes = canvas_api(:get, url)["quiz_submissions"].select do |s|
+      s['user_id'].to_s == params['custom_canvas_user_id'] &&
+      s['kept_score'].to_i >= 14
     end
 
-    if scores.select{|s| s.to_i > 90}.any?
+    if passed_quizzes.any?
       @pass = true
+      @name = params['lis_person_name_full']
       Resque.enqueue(CertificateWorker, (slim :certificate, :layout => false),
                      params['lis_person_contact_email_primary'])
     end
 
+    headers 'X-Frame-Options' => "ALLOW_FROM #{settings.canvas_url}"
     slim :index, :layout => false
   end
 
